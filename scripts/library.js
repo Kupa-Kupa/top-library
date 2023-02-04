2d25650e6e6dfe85c4cfc0162f2a87a809594126
cooper <cooper.elliott30@gmail.com>
2023-02-04 20:18:17 +1100

    Add skelton for side bar

---
+++ scripts/library.js
@@ -0,0 +1,36 @@
+// array to store book objects
+let library = [];
+
+// Book constructor function
+function Book(title, author, currentPage, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.currentPage = currentPage;
+  this.pages = pages;
+  this.percentage = (currentPage / pages) * 100;
+  this.read = read;
+}
+
+// info method
+Book.prototype.info = function () {
+  let status = '';
+  if (this.read.toLowerCase() === 'yes') {
+    status = 'read';
+  } else if (this.read.toLowerCase() === 'no') {
+    status = 'not read yet';
+  }
+  return `${this.title} by ${this.author}, ${
+    this.pages
+  } pages, ${status}. You are ${this.percentage.toFixed(
+    2
+  )}% of the way through.`;
+};
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 30, 295, 'no');
+console.log(theHobbit.info());
+console.log(theHobbit.percentage);
+console.log(theHobbit.author);
+console.log(theHobbit);
+
+// function to add book object to library
+function addBookToLibrary() {}
